f12eac32c62383b69e0407954686dbb99b583e92
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-05-10 06:56:16 +0000

    first push

---
+++ taskapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import TaskModel
+
+class TaskModelForm(forms.ModelForm):
+    class Meta:
+        model = TaskModel
+        fields = ['task',]
+
+class TaskUpdateForm(forms.ModelForm):
+    class Meta:
+        model = TaskModel
+        fields = '__all__'
---
+++ test.py
@@ -0,0 +1,8 @@
+
+
+
+
+
+names = {1,2,3,4,5}
+
+print(type(names))
